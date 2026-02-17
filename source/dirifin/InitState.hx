96e14047577ccd5b885e5fdda7c1100668148707
Macohi <sinconsistencia@gmail.com>
2026-02-16 18:11:51 -0600

    MusicManager plugin

--- source/dirifin/InitState.hx
+++ source/dirifin/InitState.hx
@@ -5,6 +5,7 @@ import macohi.debugging.CrashHandler;
 import macohi.funkin.koya.backend.AssetPaths;
 import macohi.funkin.koya.backend.plugins.Cursor;
 import macohi.overrides.MState;
+import macohi.util.MusicManager;
 
 class InitState extends MState
 {
@@ -19,6 +20,9 @@ class InitState extends MState
 
 		FlxG.plugins.addPlugin(new Cursor());
 		Cursor.cursorVisible = false;
+
+		FlxG.plugins.addPlugin(new MusicManager());
+		MusicManager.tracks = ['dungeon drif'];
 
 		AssetPaths.soundExt = 'wav';
 
