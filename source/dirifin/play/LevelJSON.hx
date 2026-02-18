5a0369e74fb6bb1d02a098cd4cbe9555708b65f9
Macohi <sinconsistencia@gmail.com>
2026-02-17 19:35:38 -0600

    parsin level jsons

--- source/dirifin/play/LevelJSON.hx
+++ source/dirifin/play/LevelJSON.hx
@@ -1,6 +1,10 @@
 package dirifin.play;
 
+import haxe.Json;
+import macohi.funkin.koya.backend.AssetPaths;
+import macohi.funkin.koya.backend.KoyaAssets;
 import macohi.typedefs.MinMaxTypedef;
+import macohi.util.WindowUtil;
 
 typedef EnemySpawningData =
 {
@@ -8,7 +12,40 @@ typedef EnemySpawningData =
 	spawn_chance:MinMaxTypedef
 }
 
-typedef LevelJSON =
+typedef LevelJSONData =
 {
 	enemySpawning:EnemySpawningData
+}
+
+class LevelJSONClass
+{
+	public static var DEFAULT_LEVEL_JSON:LevelJSONData = {
+		enemySpawning: {
+			dupe_direction_chance: 10.0,
+			spawn_chance: {
+				max: 3.0,
+				min: 0.0
+			}
+		}
+	}
+
+	public static function loadLevelJSON(level:String):LevelJSONData
+	{
+		var levelJSONPath:String = AssetPaths.json('data/levels/$level');
+
+		if (KoyaAssets.exists(levelJSONPath))
+		{
+			try
+			{
+				return Json.parse(KoyaAssets.getText(levelJSONPath));
+			}
+			catch (e)
+			{
+				WindowUtil.alert('FAILED TO PARSE LEVEL JSON : $level', 'Cant parse JSON for level: $level\n\n${e.message}');
+				trace(e.message);
+			}
+		}
+
+		return DEFAULT_LEVEL_JSON;
+	}
 }
