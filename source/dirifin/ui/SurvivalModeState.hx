4941311845b9e6c9f46eab79d33746400b4f3cb3
Macohi <sinconsistencia@gmail.com>
2026-02-21 15:13:57 -0600

    player float effect

--- source/dirifin/ui/SurvivalModeState.hx
+++ source/dirifin/ui/SurvivalModeState.hx
@@ -5,14 +5,17 @@ import dirifin.play.PlayState;
 
 class SurvivalModeState extends SpinningPlayerState
 {
-	override function create()
-	{
-		super.create();
-	}
+	public var tick:Int = 0;
+
+	public var speed:Float = 100;
+	public var height:Float = 25;
+
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		player.y = Math.sin(tick * speed) * height;
 
 		MenuStateControls.controlsOther(function()
 		{
