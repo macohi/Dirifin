593145d7c4925893908de3845db83fedd6c7dbee
Macohi <sinconsistencia@gmail.com>
2026-02-20 22:57:31 -0600

    level JSON merging

--- source/dirifin/play/LevelJSON.hx
+++ source/dirifin/play/LevelJSON.hx
@@ -1,10 +1,13 @@
 package dirifin.play;
 
 import haxe.Json;
+import macohi.backend.polymod.JsonMergeAndAppend;
 import macohi.funkin.koya.backend.AssetPaths;
 import macohi.funkin.koya.backend.KoyaAssets;
 import macohi.typedefs.MinMaxTypedef;
 import macohi.util.WindowUtil;
+
+using StringTools;
 
 typedef EnemySpawningData =
 {
@@ -39,17 +42,44 @@ class LevelJSONClass
 	public static function loadLevelJSON(level:String):LevelJSONData
 	{
 		var levelJSONPath:String = AssetPaths.json('data/levels/$level');
+		var levelJSONPathMerge:Array<String> = AssetPaths.getAllModPaths(levelJSONPath.replace('assets', '_merge'));
 
 		if (KoyaAssets.exists(levelJSONPath))
 		{
 			try
 			{
-				return Json.parse(KoyaAssets.getText(levelJSONPath));
+				var baseJson = Json.parse(KoyaAssets.getText(levelJSONPath));
+
+				for (path in levelJSONPathMerge)
+				{
+					var mergePath = path;
+
+					if (KoyaAssets.exists(mergePath))
+					{
+						try
+						{
+							var mergeJson = Json.parse(KoyaAssets.getText(mergePath));
+
+							baseJson = Json.parse(JsonMergeAndAppend.merge(
+								Json.stringify(baseJson),
+								Json.stringify(mergeJson),
+								level // id does nothing with JSONS so yeah
+							));
+						}
+						catch (e)
+						{
+							trace(e.message);
+							WindowUtil.alert('Couldnt parse $mergePath', 'Cant parse Merge JSON for level: $level\n\n${e.message}');
+						}
+					}
+				}
+
+				return baseJson;
 			}
 			catch (e)
 			{
-				WindowUtil.alert('FAILED TO PARSE LEVEL JSON : $level', 'Cant parse JSON for level: $level\n\n${e.message}');
 				trace(e.message);
+				WindowUtil.alert('FAILED TO PARSE LEVEL JSON : $level', 'Cant parse JSON for level: $level\n\n${e.message}');
 			}
 		}
 
